49f00503a9b1bf52395b8339f46c99ad771e48ca
Adam <adam.crossland@zero1.co.uk>
2023-03-14 13:50:44 +0000

    debug

--- nx/generate-workspace.js
+++ nx/generate-workspace.js
@@ -34,7 +34,8 @@ paths.forEach(path => {
                     command: 'echo "integration tests for: '+composerJson.name+'"'
                 },
                 'test:test': {
-                    command: 'echo "den location: ${DEN}"'
+                    command: '${DEN} env exec -T php-fpm ./vendor/bin/phpunit -c phpunit.nx.xml ' + path.replace('composer.json', '') + 'Test/Unit'
+                    
                 }
             },
             tags: [],
